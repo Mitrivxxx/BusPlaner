91eb8564991efe71e4fed0935d72c7ef873d5042
Mateusz <matsma2468@gmail.com>
2025-03-26 09:09:08 +0100

    migracja dj na fa

--- nuxt/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtConfig({
-  compatibilityDate: '2025-03-19',
-  devtools: { enabled: true },
-
-  modules: [
-  ],
-  
-});
--- postgres/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE mydatabase;
-CREATE USER myuser WITH PASSWORD 'mypassword';
-ALTER ROLE myuser SET client_encoding TO 'utf8';
-ALTER ROLE myuser SET default_transaction_isolation TO 'read committed';
-ALTER ROLE myuser SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
